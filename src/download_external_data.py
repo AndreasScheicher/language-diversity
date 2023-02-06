644dcffaacafb98c5e4801bd7a979ea916559d2b
AndreasScheicher <56432089+AndreasScheicher@users.noreply.github.com>
2023-02-06 16:14:53 +0100

    add downloads of english files

--- src/download_external_data.py
+++ src/download_external_data.py
@@ -8,12 +8,17 @@ import gzip
 DATA_FOLDER = os.path.join("data", "external")
 EMBEDDINGS_FOLDER = os.path.join(DATA_FOLDER, "embeddings")
 AFFECTIVE_NORMS_GER_FOLDER = os.path.join(DATA_FOLDER, "affective_norms")
+CORPUS_HISTORICAL_AM_EN = os.path.join(DATA_FOLDER, "historical_american_english")
+CONCRETENESS_RATINGS_EN = os.path.join(DATA_FOLDER, "concreteness_ratings_en")
 
 EMBEDDINGS_URL = "http://snap.stanford.edu/historical_embeddings/"
 MILLION_POST_CORPUS_URL = "https://github.com/OFAI/million-post-corpus/releases/download/v1.0.0/"
 MILLION_POST_CORPUS_FILE = "million_post_corpus.tar.bz2"
 AFFECTIVE_NORMS_GER_URL = "https://www.ims.uni-stuttgart.de/documents/ressourcen/experiment-daten/"
 AFFECTIVE_NORMS_GER_FILE = "affective_norms.txt.gz"
+CORPUS_HISTORICAL_AM_EN_URL = "http://vectors.nlpl.eu/repository/20/"
+CORPUS_HISTORICAL_AM_EN_FILE = "188.zip"
+CONCRETENESS_RATINGS_EN_URL = "http://crr.ugent.be/papers/Concreteness_ratings_Brysbaert_et_al_BRM.txt"
 
 
 def download_embeddings(EMBEDDINGS_FOLDER=EMBEDDINGS_FOLDER, EMBEDDINGS_URL=EMBEDDINGS_URL, languages = 'DE'):
@@ -99,7 +104,7 @@ def download_affective_norms_ger(folder=AFFECTIVE_NORMS_GER_FOLDER, url=AFFECTIV
         os.makedirs(folder)
 
     # Download the file using the wget module
-    wget.download(url + file)
+    wget.download(url + file, file)
 
     # Specify the output file name
     output_file = os.path.join(folder, file.rstrip('.gz'))
@@ -115,8 +120,34 @@ def download_affective_norms_ger(folder=AFFECTIVE_NORMS_GER_FOLDER, url=AFFECTIV
     os.remove(file)
 
 
+def download_corpus_historical_american_english(folder=CORPUS_HISTORICAL_AM_EN, url=CORPUS_HISTORICAL_AM_EN_URL, file=CORPUS_HISTORICAL_AM_EN_FILE):
+    # Check if the specified folder exists and create it if necessary
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # Download the file using the wget module
+    wget.download(url + file, file)
+
+    # Extract the files from the zip file to the subfolder
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(folder)
+        
+    # Delete the zip file
+    #os.remove(embedding_zip)
+
+
+def download_concreteness_en(folder=CONCRETENESS_RATINGS_EN, url=CONCRETENESS_RATINGS_EN_URL):
+    # Check if the specified folder exists and create it if necessary
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # Download the file using the wget module
+    wget.download(url, out=folder)
+
 
 if __name__ == "__main__":
-    download_million_post_corpus()
-    download_embeddings()
-    download_affective_norms_ger()
+    #download_million_post_corpus()
+    #download_embeddings()
+    #download_affective_norms_ger()
+    download_corpus_historical_american_english()
+    #download_concreteness_en()
