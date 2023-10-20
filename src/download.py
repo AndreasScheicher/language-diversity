ad22ea5b49376a101f16a29939f3b7fd1023286d
AndreasScheicher <56432089+AndreasScheicher@users.noreply.github.com>
2023-10-20 11:08:53 +0200

    clean up download.py

--- src/download.py
+++ src/download.py
@@ -3,27 +3,9 @@
 import os
 
 import zipfile
-import tarfile
-import gzip
 import wget
 
 from src import config
-
-# define folders
-#DATA_FOLDER = os.path.join("data", "external")
-#AFFECTIVE_NORMS_GER_FOLDER = os.path.join(DATA_FOLDER, "affective_norms")
-#CORPUS_HISTORICAL_AM_ENG = os.path.join(DATA_FOLDER, "historical_american_english")
-#CONCRETENESS_RATINGS_ENG = os.path.join(DATA_FOLDER, "concreteness_ratings_en")
-
-# define urls and filenames for download
-#MILLION_POST_CORPUS_URL = "https://github.com/OFAI/million-post-corpus/releases/download/v1.0.0/"
-#MILLION_POST_CORPUS_FILE = "million_post_corpus.tar.bz2"
-#AFFECTIVE_NORMS_GER_URL = "https://www.ims.uni-stuttgart.de/documents/ressourcen/experiment-daten/"
-#AFFECTIVE_NORMS_GER_FILE = "affective_norms.txt.gz"
-#CORPUS_HISTORICAL_AM_ENG_URL = "http://vectors.nlpl.eu/repository/20/"
-#CORPUS_HISTORICAL_AM_ENG_FILE = "188.zip"
-#CONCRETENESS_ENG_URL = "http://crr.ugent.be/papers/Concreteness_ratings_Brysbaert_et_al_BRM.txt"
-
 
 def download_hist_embeddings(languages = 'english',
         folder=config.EMBEDDINGS_DIR, url=config.EMBEDDINGS_URL,
